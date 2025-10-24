c9fe137356be0d04d35f8671b04a0062335a383d
Oliver Weichhold <oliver.weichhold@pm.me>
2025-10-24 17:29:54 +0200

    Icon

--- credentials/Fl2Api.credentials.ts
+++ credentials/Fl2Api.credentials.ts
@@ -3,12 +3,13 @@ import type {
 	ICredentialTestRequest,
 	ICredentialType,
 	INodeProperties,
+	Icon
 } from 'n8n-workflow';
 
 export class Fl2Api implements ICredentialType {
 	name = 'fl2Api';
 	displayName = 'F2 API';
-	icon = 'file:logo.svg' as unknown as any;
+	icon = 'file:logo.svg' as Icon;
 	documentationUrl = 'https://github.com/oliverw/n8n-nodes-fl2?tab=readme-ov-file#credentials';
 
 	properties: INodeProperties[] = [
